b0c6a944dd01f5f5ca346a881fa10e2f64b76f5f
V4Vince <vince.guoo@gmail.com>
2016-11-01 12:40:11 -0400

    Adds more seed data to DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,7 @@
 
 
 prop1 = Building.create("building_name" => "Stevens St", "address" => "2500 Fake St", "total_units" => "3", "current_insurance" => "2000", "current_taxes" => "2000", "user_id" => User.first.id);
+
+prop2 = Building.create("building_name" => "Branch St", "address" => "540 MoreFake St", "total_units" => "2", "current_insurance" => "2500", "current_taxes" => "2500", "user_id" => User.first.id);
+
+prop3 = Building.create("building_name" => "Woburn St", "address" => "30 Faker St", "total_units" => "", "current_insurance" => "2500", "current_taxes" => "", "user_id" => User.first.id);
